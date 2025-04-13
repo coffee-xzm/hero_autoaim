a19962c96cb0d02dd16b4e705381cc41f9d5d26e
coffee-xzm <644334593@qq.com>
2025-04-13 20:29:33 +0800

    清除了多余的.git

---
+++ src/rm_auto_aim/armor_detector/include/armor_detector/pnp_solver.hpp
@@ -0,0 +1,54 @@
+// Copyright 2022 Chen Jun
+// Licensed under the MIT License.
+
+#ifndef ARMOR_DETECTOR__PNP_SOLVER_HPP_
+#define ARMOR_DETECTOR__PNP_SOLVER_HPP_
+
+#include <geometry_msgs/msg/point.hpp>
+#include <opencv2/core.hpp>
+
+// STD
+#include <array>
+#include <vector>
+
+#include "armor_detector/armor.hpp"
+
+namespace rm_auto_aim
+{
+class PnPSolver
+{
+public:
+  PnPSolver(
+    const std::array<double, 9> & camera_matrix,
+    const std::vector<double> & distortion_coefficients);
+
+  // Get 3d position
+  bool solvePnP(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec);
+
+  // Calculate the distance between armor center and image center
+  float calculateDistanceToCenter(const cv::Point2f & image_point);
+
+  //绘图相关辅助
+  const std::vector<cv::Point3f> & getSmallArmorPoints() const { return small_armor_points_; }
+  const std::vector<cv::Point3f> & getLargeArmorPoints() const { return large_armor_points_; }
+  const cv::Mat & getCameraMatrix() const { return camera_matrix_; }
+  const cv::Mat & getDistCoeffs() const { return dist_coeffs_; }
+
+private:
+  cv::Mat camera_matrix_;
+  cv::Mat dist_coeffs_;
+
+  // Unit: mm
+  static constexpr float SMALL_ARMOR_WIDTH = 135;
+  static constexpr float SMALL_ARMOR_HEIGHT = 55;
+  static constexpr float LARGE_ARMOR_WIDTH = 225;
+  static constexpr float LARGE_ARMOR_HEIGHT = 55;
+
+  // Four vertices of armor in 3d
+  std::vector<cv::Point3f> small_armor_points_;
+  std::vector<cv::Point3f> large_armor_points_;
+};
+
+}  // namespace rm_auto_aim
+
+#endif  // ARMOR_DETECTOR__PNP_SOLVER_HPP_
